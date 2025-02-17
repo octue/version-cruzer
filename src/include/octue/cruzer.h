538578224d90fc5f89750b0ab8cad418f8378870
Juan Cruz Viotti <jv@jviotti.com>
2025-02-17 12:06:20 -0400

    Scaffold the repository to use Sourcemeta Core
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/include/octue/cruzer.h
@@ -0,0 +1,10 @@
+#ifndef OCTUE_CRUZER_H_
+#define OCTUE_CRUZER_H_
+
+#ifndef OCTUE_CRUZER_EXPORT
+#include <octue/cruzer_export.h>
+#endif
+
+namespace octue {}
+
+#endif
---
+++ test/cruzer_test.cc
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+#include <octue/cruzer.h>
+#include <sourcemeta/core/json.h>
+
+TEST(Cruzer, sample_1) { EXPECT_TRUE(true); }
