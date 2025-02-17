c46f78d42d6d0fbba9ef862199f0d7e1e77575a4
Juan Cruz Viotti <jv@jviotti.com>
2025-02-17 13:37:03 -0400

    Outline an `is_compatible_with` helper function (#3)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/cruzer.cc
+++ src/cruzer.cc
@@ -1,1 +1,29 @@
 #include <octue/cruzer.h>
+
+namespace octue {
+
+auto is_compatible_with(
+    const sourcemeta::core::JSON &left, const sourcemeta::core::JSON &right,
+    const sourcemeta::core::SchemaWalker &walker_left,
+    const sourcemeta::core::SchemaWalker &walker_right,
+    const sourcemeta::core::SchemaResolver &resolver_left,
+    const sourcemeta::core::SchemaResolver &resolver_right,
+    const std::optional<sourcemeta::core::JSON::String> &default_dialect_left,
+    const std::optional<sourcemeta::core::JSON::String> &default_dialect_right,
+    const std::optional<sourcemeta::core::JSON::String> &default_id_left,
+    const std::optional<sourcemeta::core::JSON::String> &default_id_right)
+    -> std::vector<Result> {
+  // (1) Frame both schemas for unresolved instance locations
+  sourcemeta::core::SchemaFrame frame_left{
+      sourcemeta::core::SchemaFrame::Mode::Instances};
+  frame_left.analyse(left, walker_left, resolver_left, default_dialect_left,
+                     default_id_left);
+  sourcemeta::core::SchemaFrame frame_right{
+      sourcemeta::core::SchemaFrame::Mode::Instances};
+  frame_right.analyse(right, walker_right, resolver_right,
+                      default_dialect_right, default_id_right);
+
+  return {};
+}
+
+} // namespace octue
--- src/include/octue/cruzer.h
+++ src/include/octue/cruzer.h
@@ -1,10 +1,45 @@
 #ifndef OCTUE_CRUZER_H_
 #define OCTUE_CRUZER_H_
 
+#include <sourcemeta/core/json.h>
+#include <sourcemeta/core/jsonpointer.h>
+#include <sourcemeta/core/jsonschema.h>
+
 #ifndef OCTUE_CRUZER_EXPORT
 #include <octue/cruzer_export.h>
 #endif
 
-namespace octue {}
+#include <optional> // std::optional, std::nullopt
+#include <vector>   // std::vector
+
+namespace octue {
+enum class Compatibility { Compatible, Incompatible, Annotation, Unknown };
+
+struct Result {
+  const Compatibility compatibility;
+  const sourcemeta::core::Pointer left;
+  const sourcemeta::core::Pointer right;
+};
+
+auto is_compatible_with(
+    const sourcemeta::core::JSON &left, const sourcemeta::core::JSON &right,
+    const sourcemeta::core::SchemaWalker &walker_left =
+        sourcemeta::core::schema_official_walker,
+    const sourcemeta::core::SchemaWalker &walker_right =
+        sourcemeta::core::schema_official_walker,
+    const sourcemeta::core::SchemaResolver &resolver_left =
+        sourcemeta::core::schema_official_resolver,
+    const sourcemeta::core::SchemaResolver &resolver_right =
+        sourcemeta::core::schema_official_resolver,
+    const std::optional<sourcemeta::core::JSON::String> &default_dialect_left =
+        std::nullopt,
+    const std::optional<sourcemeta::core::JSON::String> &default_dialect_right =
+        std::nullopt,
+    const std::optional<sourcemeta::core::JSON::String> &default_id_left =
+        std::nullopt,
+    const std::optional<sourcemeta::core::JSON::String> &default_id_right =
+        std::nullopt) -> std::vector<Result>;
+
+} // namespace octue
 
 #endif
