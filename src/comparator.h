94d093826a065dee5ad4a326361604d00b967525
Juan Cruz Viotti <jv@jviotti.com>
2025-02-19 11:35:52 -0400

    Move boolean schema comparisons to `comparator.h`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/comparator.h
+++ src/comparator.h
@@ -1,11 +1,13 @@
 #ifndef OCTUE_CRUZER_COMPARATOR_H_
 #define OCTUE_CRUZER_COMPARATOR_H_
 
+#include <cassert> // assert
+
 namespace octue {
 
 static auto compare(
     // Left current subschema
-    const sourcemeta::core::JSON &,
+    const sourcemeta::core::JSON &left_subschema,
     // Left keyword name
     const sourcemeta::core::JSON::String &,
     // Left keyword vocabulary
@@ -16,7 +18,7 @@ static auto compare(
     const sourcemeta::core::Pointer &left_schema_location,
 
     // Right current subschema
-    const sourcemeta::core::JSON &,
+    const sourcemeta::core::JSON &right_subschema,
     // Right keyword name
     const sourcemeta::core::JSON::String &,
     // Right keyword vocabulary
@@ -25,6 +27,14 @@ static auto compare(
     const sourcemeta::core::SchemaKeywordType &,
     // Right schema location
     const sourcemeta::core::Pointer &right_schema_location) -> octue::Result {
+
+  if (left_subschema.is_boolean() && right_subschema.is_boolean()) {
+    return {!left_subschema.to_boolean() && right_subschema.to_boolean()
+                ? Compatibility::Incompatible
+                : Compatibility::Compatible,
+            left_schema_location, right_schema_location};
+  }
+
   return {Compatibility::Unknown, left_schema_location, right_schema_location};
 }
 
