4a7e5483ec411226b37718dcaa8e1640c04faa3e
Juan Cruz Viotti <jv@jviotti.com>
2025-02-19 14:07:43 -0400

    Don't make `Result::left` an optional (#20)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/macros.h
+++ test/macros.h
@@ -27,8 +27,9 @@
                              expected_right_pointer)                           \
   EXPECT_EQ((expected_traces).at((expected_index)).compatibility,              \
             octue::Compatibility::expected_compatibility);                     \
-  EXPECT_OPTIONAL_POINTER((expected_traces).at((expected_index)).left,         \
-                          expected_left_pointer);                              \
+  EXPECT_EQ(sourcemeta::core::to_string(                                       \
+                (expected_traces).at((expected_index)).left),                  \
+            expected_left_pointer);                                            \
   EXPECT_OPTIONAL_POINTER((expected_traces).at((expected_index)).right,        \
                           expected_right_pointer);
 
