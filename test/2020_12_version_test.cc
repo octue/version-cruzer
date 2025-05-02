1a8dcfd1b21c839198afb59e75f78f284bf4a839
Juan Cruz Viotti <jv@jviotti.com>
2025-05-02 15:01:28 -0400

    Add a Draft 7 smoke test
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/2020_12_version_test.cc
+++ test/2020_12_version_test.cc
@@ -1,36 +1,7 @@
 #include <gtest/gtest.h>
 #include <octue/cruzer.h>
 
-#define EXPECT_UNKNOWN(container, expected_from, expected_to, expected_traces) \
-  const auto container{octue::cruzer::version(expected_from, expected_to)};    \
-  EXPECT_FALSE(container.version.has_value());                                 \
-  EXPECT_EQ(container.traces.size(), (expected_traces));
-
-#define EXPECT_VERSION(container, expected_from, expected_to,                  \
-                       expected_version, expected_traces)                      \
-  const auto container{octue::cruzer::version(expected_from, expected_to)};    \
-  EXPECT_TRUE(container.version.has_value());                                  \
-  EXPECT_EQ(container.version.value(),                                         \
-            octue::cruzer::SemVer::expected_version);                          \
-  EXPECT_EQ(container.traces.size(), (expected_traces));
-
-#define EXPECT_OPTIONAL_POINTER(current, expected)                             \
-  if (std::optional<std::string>{expected}.has_value()) {                      \
-    EXPECT_TRUE(current.has_value());                                          \
-    EXPECT_EQ(sourcemeta::core::to_string(current.value()),                    \
-              std::optional<std::string>{expected}.value());                   \
-  } else {                                                                     \
-    EXPECT_FALSE(current.has_value());                                         \
-  }
-
-#define EXPECT_TRACE(container, expected_index, expected_compatibility,        \
-                     expected_left_pointer, expected_right_pointer)            \
-  EXPECT_EQ(container.traces.at(expected_index).compatibility,                 \
-            octue::cruzer::Compatibility::expected_compatibility);             \
-  EXPECT_OPTIONAL_POINTER(container.traces.at(expected_index).left,            \
-                          expected_left_pointer);                              \
-  EXPECT_OPTIONAL_POINTER(container.traces.at(expected_index).right,           \
-                          expected_right_pointer);
+#include "macros.h"
 
 TEST(Cruzer_version_2020_12, add_id) {
   const auto from{sourcemeta::core::parse_json(R"JSON({
